9efd9218ba02c01049c02e33e2cdd30a5e569640
Zachary Hardin <hardinzw@gmail.com>
2019-09-30 12:51:04 -0400

    Updates

---
+++ precache-manifest.6335141d181570e4b4a00cec9f098abe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b0fde8de71c544d4a482309b45d53c",
+    "url": "/Book-Search/index.html"
+  },
+  {
+    "revision": "a4c2a71846ea3dd18ca0",
+    "url": "/Book-Search/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "820e905e8bee0da4e940",
+    "url": "/Book-Search/static/js/2.ecb762f4.chunk.js"
+  },
+  {
+    "revision": "a4c2a71846ea3dd18ca0",
+    "url": "/Book-Search/static/js/main.b8f8ccfa.chunk.js"
+  },
+  {
+    "revision": "8bfcfbf57923265db0d4",
+    "url": "/Book-Search/static/js/runtime-main.2a48cbc2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Book-Search/static/media/logo.25bf045c.svg"
+  }
+]);
